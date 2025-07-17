22eb9cdc6eb0d7e2a7ff611d610e78e05fbecec6
smileeeeeZ <zx19911118@gmail.com>
2025-07-17 12:25:36 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def wecom_verify():
+    return "WeCom callback URL is live!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
